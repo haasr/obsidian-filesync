f3020cef1951a0c6830f0ae01d602097965d66a9
Ryan Haas <haasr@mailfence.com>
2023-08-05 01:02:41 -0400

    Fixed to catch exceptions to make sure electron terminates

--- launcher.js
+++ launcher.js
@@ -1,22 +1,8 @@
 const child_process = require('child_process');
 const config = require('./config')
-const dns = require('dns');
+const dns = require('dns')
 const logging = require('./logging');
 const sync = require('./sync');
-
-
-function isConnected() {
-    return new Promise((resolve) => {
-        dns.resolve('github.com', (err) => {
-            if (err) {
-                resolve(false);
-            }
-            else {
-                resolve(true);
-            }
-        });
-    });
-}
 
 
 function launchApp() {
@@ -31,29 +17,35 @@ function launchApp() {
         logging.app_log('Exiting...');
         sync.unsetLock(config.VAULT_PATH);
         sync.pushUp(config.VAULT_PATH, config.DEVICE_ID);
-        process.exit(0);
+        process.exit(1);
     });
+}
+
+function init() {
+    let lock = sync.pullDown(config.VAULT_PATH);
+
+    if (lock == '') {
+        if (sync.setLock(config.VAULT_PATH, config.DEVICE_ID)) {
+            sync.pushUp(config.VAULT_PATH, config.DEVICE_ID, lockfile_only=true);
+            launchApp();
+        }
+    }
+    else {
+        logging.app_error(`Close Obsidian on ${lock} before launching again`);
+    }
 }
 
 
 function main () {
-    isConnected()
-    .then(() => {
-        let lock = sync.pullDown(config.VAULT_PATH);
-
-        if (lock == '') {
-            if (sync.setLock(config.VAULT_PATH, config.DEVICE_ID)) {
-                sync.pushUp(config.VAULT_PATH, config.DEVICE_ID, lockfile_only=true);
-                launchApp();
-            }
+    logging.app_info('Testing connection to github.com');
+    dns.resolve('github.com', function(err) {
+        if (err) {
+            logging.app_error("Could not reach github.com. Check your Internet connection.");
+            process.exit(1);
         }
         else {
-            logging.app_error(`Close Obsidian on ${lock} before launching again`);
+            init();
         }
-    })
-    .catch(() => {
-        logging.app_error("Unable to reach github.com. Check your Internet connection.");
-        throw new Error("Unable to reach github.com. Check your Internet connection.");
     });
 }
 
