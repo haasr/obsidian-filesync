8ff3641adef9c6a0316471588002978613da0ea8
Ryan Haas <haasr@mailfence.com>
2023-08-04 22:02:30 -0400

    Added example config

---
+++ example-config.js
@@ -0,0 +1,20 @@
+const DEVICE_ID = "Win11Desktop";
+const EXECUTABLE_PATH = `"C:\\Users\\User\\AppData\\Local\\Obsidian\\Obsidian.exe"`;
+const VAULT_PATH = `C:\\Users\\User\\Documents\\obsidian-vault`
+
+const LOG_DETAIL  = true;
+const LOG_INFO    = true;
+const LOG_WARNING = true;
+const LOG_ERROR   = true;
+
+
+module.exports = {
+    DEVICE_ID,
+    EXECUTABLE_PATH,
+    VAULT_PATH,
+    
+    LOG_DETAIL,
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR,
+}
